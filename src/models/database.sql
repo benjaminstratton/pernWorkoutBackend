0714fbac61c744579dce62a1a264ea1558f77d55
bstrat <benstratton100@gmail.com>
2023-04-12 20:30:44 -0500

    created restful routes for logging workouts and connected to private user

--- src/models/database.sql
+++ src/models/database.sql
@@ -1,18 +1,56 @@
 CREATE DATABASE workout_api;
 
 -- Download Extension "uuid-ossp"
-create extension if not exists "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+-- Create Users table
 CREATE TABLE users(
-    user_id uuid PRIMARY KEY DEFAULT
-    uuid_generate_v4(),
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_name VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL UNIQUE,
     user_password VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL,
     user_sex VARCHAR(1),
-    user_height INT
+    user_height INT DEFAULT 66
 );
 
--- Insert fake users (test)
-INSERT INTO users (user_name, user_email, user_password, date_of_birth, user_sex, user_height) VALUES ('SoggyJake', 'soggymuffin@gmail.com', 'benisbiggerthanme123!', '1999-12-19', 'M', 68);
+-- Insert test users (test)
+INSERT INTO users(user_name, user_email, user_password, date_of_birth, user_sex, user_height)
+VALUES
+('SoggyJake', 'soggymuffin@gmail.com', 'Benisbiggerthanme123!', '1999-12-19', 'M', 68),
+('Brorito', 'jalenrojas@gmail.com', 'Bentley123!', '2001-08-09', 'M', 66);
+
+-- Create Workout Log table
+CREATE TABLE workout_log(
+workout_id SERIAL PRIMARY KEY,
+user_id UUID,
+log_title TEXT NOT NULL,
+FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- Create Exercise table
+CREATE TABLE exercise(
+ex_id SERIAL PRIMARY KEY,
+workout_id SERIAL,
+ex_name VARCHAR(50),
+sets INT,
+weight INT [],
+reps INT [],
+FOREIGN KEY (workout_id) REFERENCES workout_log(workout_id)
+);
+
+-- Insert test data (test)
+INSERT INTO workout_log(user_id, log_title)
+VALUES
+('<UUID>', 'Monday, April 10th'),
+('<UUID>', 'Tuesday, April 11th');
+
+INSERT INTO exercise(workout_id, ex_name, sets, weight, reps)
+VALUES
+(1, 'Deadlift', 4, '{315, 315, 315, 315}', '{8, 8, 8, 8}'),
+(1, 'Lat Pulldown', 3, '{180, 180, 180}', '{10, 10, 10}'),
+(2, 'Bench Press', 3, '{225, 225, 225}', '{6, 6, 6}'),
+(2, 'Dips', 3, '{25, 25, 25}', '{8, 8, 8}');
+
+-- Testing Join (test)
+SELECT * FROM users AS u LEFT JOIN workout_log AS w ON u.user_id = w.user_id LEFT JOIN exercise AS e ON e.workout_id = w.workout_id WHERE u.user_id = '<UUID>';

4140a73385374748869070b3a6f26e3de301645d
bstrat <benstratton100@gmail.com>
2023-04-15 18:57:21 -0500

    change database and routes to fit running specs since I had trouble with mapping through multiple tables in react

--- src/models/database.sql
+++ src/models/database.sql
@@ -4,7 +4,7 @@ CREATE DATABASE workout_api;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Create Users table
-CREATE TABLE users(
+CREATE TABLE users (
     user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_name VARCHAR(255) NOT NULL,
     user_email VARCHAR(255) NOT NULL UNIQUE,
@@ -15,28 +15,51 @@ CREATE TABLE users(
 );
 
 -- Insert test users (test)
-INSERT INTO users(user_name, user_email, user_password, date_of_birth, user_sex, user_height)
+INSERT INTO users (user_name, user_email, user_password, date_of_birth, user_sex, user_height)
 VALUES
 ('SoggyJake', 'soggymuffin@gmail.com', 'Benisbigger123!', '1999-12-19', 'M', 68),
 ('Brorito', 'jalenrojas@gmail.com', 'Bentley123!', '2001-08-09', 'M', 66);
 
+
+CREATE TABLE runs (
+    run_id SERIAL PRIMARY KEY,
+    user_id UUID,
+    date DATE NOT NULL,
+    distance INT NOT NULL,
+    time INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+INSERT INTO runs (user_id, date, distance, time)
+VALUES 
+('<UUID>', '2023-04-10', 3, 18),
+('<UUID>', '2023-04-11', 4, 26);
+
+-- Test Join (test)
+SELECT * FROM users AS u LEFT JOIN runs AS r ON u.user_id = r.user_id WHERE u.user_id = '<UUID>';
+
+
+
+
+
+-- Can't get below to work with react, so it is not being added for now
 -- Create Workout Log table
 CREATE TABLE workout_log(
-workout_id SERIAL PRIMARY KEY,
-user_id UUID,
-log_title TEXT NOT NULL,
-FOREIGN KEY (user_id) REFERENCES users(user_id)
+    workout_id SERIAL PRIMARY KEY,
+    user_id UUID,
+    log_title TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 -- Create Exercise table
 CREATE TABLE exercise(
-ex_id SERIAL PRIMARY KEY,
-workout_id SERIAL,
-ex_name VARCHAR(50),
-sets INT,
-weight INT [],
-reps INT [],
-FOREIGN KEY (workout_id) REFERENCES workout_log(workout_id) on DELETE Cascade
+    ex_id SERIAL PRIMARY KEY,
+    workout_id SERIAL,
+    ex_name VARCHAR(50),
+    sets INT,
+    weight INT [],
+    reps INT [],
+    FOREIGN KEY (workout_id) REFERENCES workout_log(workout_id) on DELETE Cascade
 );
 
 -- Insert test data (test)
@@ -54,3 +77,5 @@ VALUES
 
 -- Testing Join (test)
 SELECT * FROM users AS u LEFT JOIN workout_log AS w ON u.user_id = w.user_id LEFT JOIN exercise AS e ON e.workout_id = w.workout_id WHERE u.user_id = '<UUID>';
+
+SELECT * FROM workout_log AS w LEFT JOIN exercise AS e ON w.workout_id = e.workout_id WHERE w.user_id = '<UUID>';
--- src/routes/dash.js
+++ src/routes/dash.js
@@ -4,11 +4,11 @@ const router = require("express").Router();
 const pool = require("../config/db");
 const authorization = require("../middlewares/authorization");
 
-// Get user_name and ALL workout logs
+// Get user_name and ALL runs
 router.get("/", authorization, async (req, res) => {
   try {
     const user = await pool.query(
-      "SELECT u.user_name, w.workout_id, w.log_title, e.ex_id, e.ex_name, e.sets, e.weight, e.reps FROM users AS u LEFT JOIN workout_log AS w ON u.user_id = w.user_id LEFT JOIN exercise AS e ON e.workout_id = w.workout_id WHERE u.user_id = $1",
+      "SELECT u.user_name, r.run_id, r.date, r.distance, r.time FROM users AS u LEFT JOIN runs AS r ON u.user_id = r.user_id WHERE u.user_id = $1",
       [req.user.id]
     );
     res.json(user.rows);
@@ -18,100 +18,50 @@ router.get("/", authorization, async (req, res) => {
   }
 });
 
-// Create workout log
-router.post("/workout", authorization, async (req, res) => {
+// Create run workout
+router.post("/runs", authorization, async (req, res) => {
   try {
-    const { title } = req.body;
-    const newLog = await pool.query(
-      "INSERT INTO workout_log(user_id, log_title) VALUES ($1, $2) RETURNING *",
-      [req.user.id, title]
+    const { date, distance, time } = req.body;
+    const newRun = await pool.query(
+      "INSERT INTO runs (user_id, date, distance, time) VALUES ($1, $2, $3, $4) RETURNING *",
+      [req.user.id, date, distance, time]
     );
-    res.json(newLog.rows[0]);
+    res.json(newRun.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 });
 
-// Create exercise
-router.post("/exercise/:id", authorization, async (req, res) => {
+// Update run workout
+router.put("/runs/:id", authorization, async (req, res) => {
   try {
     const { id } = req.params;
-    const { exercise, sets, weight, reps } = req.body;
-    const newExercise = await pool.query(
-      "INSERT INTO exercise(workout_id, ex_name, sets, weight, reps) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-      [id, exercise, sets, weight, reps]
+    const { date, distance, time } = req.body;
+    const updateRun = await pool.query(
+      "UPDATE runs SET date = $1, distance = $2, time = $3 WHERE run_id = $4 AND user_id = $5 RETURNING *",
+      [date, distance, time, id, req.user.id]
     );
-    res.json(newExercise.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-// Update workout log
-router.put("/workout/:id", authorization, async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { title } = req.body;
-    const updateLogTitle = await pool.query(
-      "UPDATE workout_log SET log_title = $1 WHERE workout_id = $2 AND user_id = $3 RETURNING *",
-      [title, id, req.user.id]
-    );
-    if (updateLogTitle.rows.length === 0) {
-      return res.json("This log is not yours!");
+    if (updateRun.rows.length === 0) {
+      return res.json("This run is not yours!");
     }
-    res.json("Log updated!");
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-// Update exercise
-router.put("/exercise/:id", authorization, async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { exercise, sets, weight, reps } = req.body;
-    const addSet = await pool.query(
-      "UPDATE exercise SET exercise = $1, sets = $2, weight = $3, reps = $4 WHERE ex_id = $5 RETURNING *",
-      [exercise, sets, weight, reps, id]
-    );
-    if (addSet.rows.length === 0) {
-      return res.json("Failed");
-    }
-    res.json("Added Sets");
+    res.json("Run updated!");
   } catch (err) {
     console.error(err.message);
   }
 });
 
 // Delete workout log
-router.delete("/workout/:id", authorization, async (req, res) => {
+router.delete("/runs/:id", authorization, async (req, res) => {
   try {
     const { id } = req.params;
-    const deleteLog = await pool.query(
-      "DELETE FROM workout_log WHERE workout_id = $1 AND user_id = $2 RETURNING *",
+    const deleteRun = await pool.query(
+      "DELETE FROM runs WHERE run_id = $1 AND user_id = $2 RETURNING *",
       [id, req.user.id]
     );
-    if (deleteLog.rows.length === 0) {
-      return res.json("This log is not yours!");
+    if (deleteRun.rows.length === 0) {
+      return res.json("This run is not yours!");
     }
-    res.json("Log deleted!");
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-// Delete exercise
-router.delete("/exercise/:id", authorization, async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deleteExercise = await pool.query(
-      "DELETE FROM exercise WHERE ex_id = $1 RETURNING *",
-      [id]
-    );
-    if (deleteExercise.rows.length === 0) {
-      return res.json("Failed");
-    }
-    res.json("Exercise deleted!");
+    res.json("Run deleted!");
   } catch (err) {
     console.error(err.message);
   }

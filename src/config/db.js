c3c207520dbc293308dd1ca17424b30b36059651
bstrat <benstratton100@gmail.com>
2023-04-18 18:57:42 -0500

    fixing dev/prod structure

--- src/config/db.js
+++ src/config/db.js
@@ -3,27 +3,21 @@ require("dotenv").config()
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const devConnect = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+}
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString: isProduction ? process.env.DATABASE_URL : devConnect,
   ssl: {
       rejectUnauthorized: false,
   },
 });
-
-// const devConfig = {
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASSWORD,
-//   host: process.env.PG_HOST,
-//   port: process.env.PG_PORT,
-//   database: process.env.PG_DATABASE,
-// }
-
-// const proConfig = {
-//   connectionString: process.env.DATABASE_URL
-// }
-
-// const pool = new Pool(devConfig);
 
 module.exports = pool;
--- src/models/database.sql
+++ src/models/database.sql
@@ -1,4 +1,7 @@
+-- Create database and connect to it
+DROP DATABASE IF EXISTS workout_api;
 CREATE DATABASE workout_api;
+\c workout_api;
 
 -- Download Extension "uuid-ossp"
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -30,52 +33,48 @@ CREATE TABLE runs (
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-INSERT INTO runs (user_id, date, distance, time)
-VALUES 
-('<UUID>', '2023-04-10', 3, 18),
-('<UUID>', '2023-04-11', 4, 26);
+-- INSERT INTO runs (user_id, date, distance, time)
+-- VALUES 
+-- ('<UUID>', '2023-04-10', 3, 18),
+-- ('<UUID>', '2023-04-11', 4, 26);
 
 -- Test Join (test)
-SELECT * FROM users AS u LEFT JOIN runs AS r ON u.user_id = r.user_id WHERE u.user_id = '<UUID>';
-
-
-
-
+-- SELECT * FROM users AS u LEFT JOIN runs AS r ON u.user_id = r.user_id WHERE u.user_id = '<UUID>';
 
 -- Can't get below to work with react, so it is not being added for now
 -- Create Workout Log table
-CREATE TABLE workout_log(
-    workout_id SERIAL PRIMARY KEY,
-    user_id UUID,
-    log_title TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
+-- CREATE TABLE workout_log(
+--     workout_id SERIAL PRIMARY KEY,
+--     user_id UUID,
+--     log_title TEXT NOT NULL,
+--     FOREIGN KEY (user_id) REFERENCES users(user_id)
+-- );
 
 -- Create Exercise table
-CREATE TABLE exercise(
-    ex_id SERIAL PRIMARY KEY,
-    workout_id SERIAL,
-    ex_name VARCHAR(50),
-    sets INT,
-    weight INT [],
-    reps INT [],
-    FOREIGN KEY (workout_id) REFERENCES workout_log(workout_id) on DELETE Cascade
-);
+-- CREATE TABLE exercise(
+--     ex_id SERIAL PRIMARY KEY,
+--     workout_id SERIAL,
+--     ex_name VARCHAR(50),
+--     sets INT,
+--     weight INT [],
+--     reps INT [],
+--     FOREIGN KEY (workout_id) REFERENCES workout_log(workout_id) on DELETE Cascade
+-- );
 
 -- Insert test data (test)
-INSERT INTO workout_log(user_id, log_title)
-VALUES
-('<UUID>', 'Monday, June 10th'),
-('<UUID>', 'Tuesday, June 11th');
+-- INSERT INTO workout_log(user_id, log_title)
+-- VALUES
+-- ('<UUID>', 'Monday, June 10th'),
+-- ('<UUID>', 'Tuesday, June 11th');
 
-INSERT INTO exercise(workout_id, ex_name, sets, weight, reps)
-VALUES
-(3, 'Deadlift', 4, '{315, 315, 315, 315}', '{8, 8, 8, 8}'),
-(3, 'Lat Pulldown', 3, '{180, 180, 180}', '{10, 10, 10}'),
-(4, 'Bench Press', 3, '{225, 225, 225}', '{6, 6, 6}'),
-(4, 'Dips', 3, '{25, 25, 25}', '{8, 8, 8}');
+-- INSERT INTO exercise(workout_id, ex_name, sets, weight, reps)
+-- VALUES
+-- (3, 'Deadlift', 4, '{315, 315, 315, 315}', '{8, 8, 8, 8}'),
+-- (3, 'Lat Pulldown', 3, '{180, 180, 180}', '{10, 10, 10}'),
+-- (4, 'Bench Press', 3, '{225, 225, 225}', '{6, 6, 6}'),
+-- (4, 'Dips', 3, '{25, 25, 25}', '{8, 8, 8}');
 
 -- Testing Join (test)
-SELECT * FROM users AS u LEFT JOIN workout_log AS w ON u.user_id = w.user_id LEFT JOIN exercise AS e ON e.workout_id = w.workout_id WHERE u.user_id = '<UUID>';
+-- SELECT * FROM users AS u LEFT JOIN workout_log AS w ON u.user_id = w.user_id LEFT JOIN exercise AS e ON e.workout_id = w.workout_id WHERE u.user_id = '<UUID>';
 
-SELECT * FROM workout_log AS w LEFT JOIN exercise AS e ON w.workout_id = e.workout_id WHERE w.user_id = '<UUID>';
+-- SELECT * FROM workout_log AS w LEFT JOIN exercise AS e ON w.workout_id = e.workout_id WHERE w.user_id = '<UUID>';
